dd74f937e8369889ee9dc54717d0ab715182eae7
Antoine LEBOISSELIER <contact@aleboisselier.fr>
2016-03-22 16:34:35 +0100

    Completed WebGate

--- src/com/qmp/admin/utils/WebGate.java
+++ src/com/qmp/admin/utils/WebGate.java
@@ -13,18 +13,39 @@ import com.google.gson.Gson;
 public class WebGate {
 	private Map<String, String> tabCorr;
 	private String baseUrl;
+	private Gson gson;
 
 	public WebGate() {
-		baseUrl = "http://127.0.0.1:8080/Quiz-Rest/rest/";
+		baseUrl = "http://127.0.0.1:8080/QMP-Rest/rest/";
 		tabCorr = new HashMap<>();
 		tabCorr.put("Utilisateur", "user");
+		tabCorr.put("Domaine", "domain");
+		tabCorr.put("Groupe_utilisateur", "usergroup");
+		tabCorr.put("Groupe", "group");
+		tabCorr.put("Question", "question");
+		tabCorr.put("Questionnaire", "quizz");
+		tabCorr.put("Rang", "rank");
+		tabCorr.put("Reponse", "answer");
+		
+		gson = MyGsonBuilder.create();
 	}
 
-	public <T> List<T> getAll(Class<T> clazz) throws ClientProtocolException, IOException {
+	public <T> List<T> getAll(Class<T> clazz, int cd) throws ClientProtocolException, IOException {
+		if(cd < 0) cd = 1;
+		
 		List<T> result = new ArrayList<T>();
-		String jsonUsers = HttpUtils.getHTML(baseUrl + tabCorr.get(clazz.getSimpleName()) + "/all");
-		Gson gson = MyGsonBuilder.create();
-		result = gson.fromJson(jsonUsers, new ListType<T>(clazz));
+		String jsonObjects = HttpUtils.getHTML(baseUrl + tabCorr.get(clazz.getSimpleName()) + "/all");
+		result = gson.fromJson(jsonObjects, new ListType<T>(clazz));
+		return result;
+	}
+	
+	public <T> T getOne(Class<T> clazz, int id, int cd) throws ClientProtocolException, IOException {
+		if(id < 0) return null;
+		if(cd < 0) cd = 1;
+
+		String jsonObject = HttpUtils.getHTML(baseUrl + tabCorr.get(clazz.getSimpleName()) + "/id/");
+		T result = gson.fromJson(jsonObject, clazz.getClass());
+		
 		return result;
 	}
 
