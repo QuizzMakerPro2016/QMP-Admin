1edbfa8aad01c49c095a685fe8d9bc706e42dcff
NiBrossault <ni.brossault@laposte.net>
2016-04-19 16:45:30 +0200

    view quizz in tableView

--- src/com/qmp/admin/controllers/QuizzHomeController.java
+++ src/com/qmp/admin/controllers/QuizzHomeController.java
@@ -1,11 +1,15 @@
 package com.qmp.admin.controllers;
 
+import com.qmp.admin.MainApp;
 import com.qmp.admin.models.Domaine;
 import com.qmp.admin.models.Questionnaire;
+import com.qmp.admin.models.Rang;
 import com.qmp.admin.models.Utilisateur;
 import com.sun.media.jfxmedia.logging.Logger;
 
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -30,10 +34,17 @@ public class QuizzHomeController extends Controller{
     @FXML
     private Button btnEditQuizz;
     
+    @Override
+    public void setMainApp(MainApp mainApp) {
+    	super.setMainApp(mainApp);
+    	ObservableList<Questionnaire> quizzObs = mainApp.getWebGate().getList(Questionnaire.class);
+    	tableQuizzList.setItems(quizzObs);
+    }
+    
 	@FXML
 	public void initialize(){
 		btnRemQuizz.setVisible(false);
-		btnEditQuizz.setVisible(false);
+		btnEditQuizz.setVisible(true);
 		
 		nameColumn.setCellValueFactory((CellDataFeatures<Questionnaire, String> feature) -> {
 			Questionnaire quizz = feature.getValue();
@@ -43,11 +54,21 @@ public class QuizzHomeController extends Controller{
 			Questionnaire quizz = feature.getValue();
 			return new SimpleObjectProperty<>(quizz.getDomaine().getLibelle());
 		});
-		/*dateColumn.setCellValueFactory((CellDataFeatures<Questionnaire, String> feature) -> {
+		dateColumn.setCellValueFactory((CellDataFeatures<Questionnaire, String> feature) -> {
 			Questionnaire quizz = feature.getValue();
-			return new SimpleObjectProperty<>(quizz.getDate());
-		});*/
+			return new SimpleObjectProperty<>(quizz.getDate().toString());
+		});	
 	}
+
+    /**
+	 * Edit Quizz
+	 * @param event
+	 */
+    @FXML
+    void handleEdit(ActionEvent event) {
+    	QuizzController c = (QuizzController) gUtils.switchView("QuizzLayout");
+    	c.setQuizz(tableQuizzList.getSelectionModel().getSelectedItem());
+    }
 	
 	/**
 	 * Add Quizz
@@ -55,15 +76,6 @@ public class QuizzHomeController extends Controller{
 	 */
     @FXML
     void handleAdd(ActionEvent event) {
-
-    }
-    
-    /**
-	 * Edit Quizz
-	 * @param event
-	 */
-    @FXML
-    void handleEdit(ActionEvent event) {
 
     }
     
