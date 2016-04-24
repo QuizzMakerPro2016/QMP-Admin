d1a822af2b7dd0d7402b9ce2fd768799e7d34bff
Antoine LEBOISSELIER <contact@aleboisselier.fr>
2016-04-24 19:28:30 +0200

    Increased Logger class
    Added Object Logging + loacle date print + milliseconds display

--- src/com/qmp/admin/utils/Logger.java
+++ src/com/qmp/admin/utils/Logger.java
@@ -1,15 +1,40 @@
 package com.qmp.admin.utils;
 
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 
 public class Logger {
 	
+	private static String date(){
+		LocalDateTime date = LocalDateTime.now();
+		String nano = String.valueOf(date.getNano());
+		String text = date.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)) + ":" + nano.substring(0, 4) ;
+		return "[" + text + "] ";
+	}
+	
 	public static void log(String str){
-		System.out.println("[" + new Date(System.currentTimeMillis()).toString() + "] " + str);
+		System.out.println(date() + str);
+	}
+	
+	public static void log(Object o){
+		log(o.toString());
 	}
 	
 	public static void error(String str){
-		System.err.println("ERROR - [" + new Date(System.currentTimeMillis()).toString() + "] " + str);
+		System.err.println(date() + "ERROR: " + str);
+	}
+	
+	public static void error(Object o){
+		error(o.toString());
+	}
+	
+	public static void warn(String str){
+		System.err.println(date() + "WARNING: " + str);
+	}
+	
+	public static void warn(Object o){
+		error(o.toString());
 	}
 
 }
