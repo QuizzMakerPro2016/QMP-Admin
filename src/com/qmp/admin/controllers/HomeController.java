7dc9c30045a0dcf5d87b451592c20addbc95ed24
Hugo PALLA <hugo.palla@gmail.com>
2016-04-27 11:57:40 +0200

    Making home layout great again

--- src/com/qmp/admin/controllers/HomeController.java
+++ src/com/qmp/admin/controllers/HomeController.java
@@ -1,5 +1,23 @@
 package com.qmp.admin.controllers;
 
+import com.qmp.admin.models.Questionnaire;
+import com.qmp.admin.models.Utilisateur;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class HomeController extends Controller{
-	
+
+    @FXML
+    private TableView<Utilisateur> userList;
+
+    @FXML
+    private TableColumn<Utilisateur, String> userColumn;
+
+    @FXML
+    private TableView<Questionnaire> quizzList;
+
+    @FXML
+    private TableColumn<Questionnaire, String> quizzColumn;
 }
